ed0b24d842394c6b572f292e45b54561eee5c2de
dafiraone <irawandaffa7@gmail.com>
2025-01-13 23:21:00 +0700

    to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-export default {
+import { defineConfig } from "vite"
+
+export default defineConfig({
     // root: "src/",
     // publicDir: "../public",
     // base: "./"
-    base: "/thesolarsystemproject/"
-}
+    base: "/thesolarsystemproject"
+})
