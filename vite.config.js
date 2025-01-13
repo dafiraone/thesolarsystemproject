067b28125e2419f606cb182c816215767e516350
dafiraone <irawandaffa7@gmail.com>
2025-01-13 23:25:11 +0700

    upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    // root: "src/",
-    // publicDir: "../public",
-    // base: "./"
+    // root: "./",
+    // publicDir: "./public",
+    // base: "./src"
     base: "/thesolarsystemproject"
 })
